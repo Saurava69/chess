1bcaab7c6a9f8bbeabd0b779f64d2eb3fbf4687b
Kumar <saurav.kumar07@sap.com>
2025-11-01 19:11:18 +0530

    fixing error

---
+++ shared/src/types/lodash-es.d.ts
@@ -0,0 +1,18 @@
+declare module "lodash-es" {
+    export function meanBy<T>(collection: ArrayLike<T>, iteratee: (item: T) => number): number;
+    export function maxBy<T>(collection: ArrayLike<T>, iteratee: (item: T) => number): T | undefined;
+    export function minBy<T>(collection: ArrayLike<T>, iteratee: (item: T) => number): T | undefined;
+    export function uniq<T>(array: ArrayLike<T>): T[];
+    export function isEqual(value: any, other: any): boolean;
+    export function xorWith<T>(array: T[], ...args: any[]): T[];
+    export function capitalize(string: string): string;
+    export function differenceWith<T>(array: T[], values: T[], comparator?: (arrVal: T, othVal: T) => boolean): T[];
+    export function uniqueId(prefix?: string): string;
+    export function trim(string: string, chars?: string): string;
+    export function range(start: number, end?: number, step?: number): number[];
+    export function sample<T>(collection: T[]): T | undefined;
+    export function times<T>(n: number, iteratee: (num: number) => T): T[];
+    export function round(number: number, precision?: number): number;
+    export function clone<T>(value: T): T;
+    export function cloneDeep<T>(value: T): T;
+}
